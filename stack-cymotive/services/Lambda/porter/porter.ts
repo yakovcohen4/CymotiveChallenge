fd1e6b82efef4e56087cfdf4d95a92ced2c4dcd0
yakov cohen <yakovc1431@gmail.com>
2022-04-04 19:46:55 +0300

    # Create porter lambda

---
+++ stack-cymotive/services/Lambda/porter/porter.ts
@@ -0,0 +1,33 @@
+import { APIGatewayProxyEvent } from 'aws-lambda';
+import * as AWS from 'aws-sdk';
+const s3 = new AWS.S3({ region: 'eu-west-1' });
+const { nanoid } = require('nanoid');
+
+const bucketName = process.env.BUCKET || 'cymotive-reports';
+
+exports.handler = async (event: APIGatewayProxyEvent) => {
+  const params = {
+    ContentType: 'application/json',
+    Bucket: bucketName as string,
+    Body: '',
+    Key: '',
+  };
+
+  if (!event.body) {
+    throw new Error('Please Add Body');
+  }
+  const vehicles = JSON.parse(event.body);
+
+  for (let vehicle of vehicles) {
+    params.Body = JSON.stringify(vehicle);
+    params.Key = `report-${nanoid()}.json`;
+
+    try {
+      await s3.putObject(params).promise();
+    } catch (error) {
+      console.log(error);
+      return error;
+    }
+  }
+  return 'succeeded';
+};
