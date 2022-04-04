ff7616594b53c0c693ad93440f4c6dcd9ec3bdfa
yakov cohen <yakovc1431@gmail.com>
2022-04-04 16:42:19 +0300

    # Create cdk typescript app

---
+++ stack-cymotive/test/stack-cymotive.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as StackCymotive from '../lib/stack-cymotive-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/stack-cymotive-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new StackCymotive.StackCymotiveStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
