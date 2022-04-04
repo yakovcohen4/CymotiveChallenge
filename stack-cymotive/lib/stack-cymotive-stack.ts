1e3c2d616e317a774da8611b163e1590b156fc6b
yakov cohen <yakovc1431@gmail.com>
2022-04-04 17:58:13 +0300

    # Create Rest Api - Api Gateway

--- stack-cymotive/lib/stack-cymotive-stack.ts
+++ stack-cymotive/lib/stack-cymotive-stack.ts
@@ -2,15 +2,18 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+// Api Gateway
+import { RestApi, Cors, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
+
 export class StackCymotiveStack extends Stack {
+  private api = new RestApi(this, 'cymotiveApi', {
+    defaultCorsPreflightOptions: {
+      allowOrigins: Cors.ALL_ORIGINS,
+      allowMethods: Cors.ALL_ORIGINS,
+    },
+  });
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'StackCymotiveQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
   }
 }
