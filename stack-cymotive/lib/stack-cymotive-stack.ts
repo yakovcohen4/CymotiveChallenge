bf95cf96b7df53f82626ec63952c916fd4bba0a5
yakov cohen <yakovc1431@gmail.com>
2022-04-11 19:27:59 +0300

    # Create ingest Lambda

--- stack-cymotive/lib/stack-cymotive-stack.ts
+++ stack-cymotive/lib/stack-cymotive-stack.ts
@@ -2,6 +2,7 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 // Lambda Nodejs
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { S3EventSource } from 'aws-cdk-lib/aws-lambda-event-sources';
 // Api Gateway
 import { RestApi, Cors, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 // Generic Services - DynamoDB-Table / s3-Bucket / Iam-Role
@@ -15,6 +16,8 @@ import {
 import { GenericTable } from '../services/DynamoDB/GenericTable';
 import { GenericBucket } from '../services/Bucket/GenericBucket';
 import { GenericRole } from '../services/Role/GenericRole';
+import { EventType } from 'aws-cdk-lib/aws-s3';
+
 import * as path from 'path';
 
 export class StackCymotiveStack extends Stack {
@@ -87,5 +90,76 @@ export class StackCymotiveStack extends Stack {
     porterLambdaResourceAddReport.addMethod('Post', porterLambdaIntegration);
 
     // ***** porter Lambda *****
+
+    // ***** ingest Lambda *****
+
+    // Create ingest role
+    const ingestRole = new Role(this, 'ingest-role-cdk', {
+      assumedBy: new ServicePrincipal('lambda.amazonaws.com'),
+      roleName: 'CymotiveIngestRole',
+      inlinePolicies: {
+        PorterPolicy: new PolicyDocument({
+          statements: [
+            // Provides read only access cymotiveReportsBucket
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: [
+                's3:Get*',
+                's3:List*',
+                's3-object-lambda:Get*',
+                's3-object-lambda:List*',
+              ],
+              resources: [this.cymotiveReportsBucket.bucket.bucketArn],
+            }),
+            // Permission to put objects in DynamoDB-Table
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: ['dynamodb:PutItem'],
+              resources: [this.cymotiveTable.table.tableArn],
+            }),
+            // Permission to put objects in S3-Bucket
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: ['s3:PutObject'],
+              resources: [this.cymotiveReportsBucket.bucket.bucketArn],
+            }),
+            // CloudWatch policy
+            new PolicyStatement({
+              effect: Effect.ALLOW,
+              actions: [
+                'logs:CreateLogStream',
+                'logs:CreateLogGroup',
+                'logs:PutLogEvents',
+              ],
+              resources: [
+                `arn:aws:logs:${this.region}:${this.account}:log-group:/aws/lambda/*:*:*`,
+              ],
+            }),
+          ],
+        }),
+      },
+    });
+
+    // Create Lambda
+    const ingestLambdaNodejs = new NodejsFunction(this, 'ingest', {
+      functionName: 'cymotive-ingest',
+      entry: path.join(__dirname, './../services/Lambda/ingest/ingest.ts'),
+      handler: 'handler',
+      role: ingestRole,
+      environment: {
+        TABLE: this.cymotiveTable.table.tableName,
+      },
+    });
+
+    // S3 Bucket event source
+    const eventS3 = new S3EventSource(this.cymotiveReportsBucket.bucket, {
+      events: [EventType.OBJECT_CREATED_PUT],
+      filters: [{ suffix: '.json' }], // optional
+    });
+
+    // event source to ingest Lambda - to ingest data from S3
+    ingestLambdaNodejs.addEventSource(eventS3);
+
+    // ***** ingest Lambda *****
   }
 }
