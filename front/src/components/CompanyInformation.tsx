ae4f9d3d29ac68f836aa6dfe5ba04776dec7a711
yakov cohen <yakovc1431@gmail.com>
2022-04-25 17:50:44 +0300

    # Create company information comp

---
+++ front/src/components/CompanyInformation.tsx
@@ -0,0 +1,24 @@
+function CompanyInformation() {
+  return (
+    <div className="company-information">
+      <h2>About Cymotive</h2>
+      <p>
+        CYMOTIVE operates under the assumption that connectivity is a game
+        changer for the automotive industry. Our vision is to make the
+        automotive industry a safe place.
+      </p>
+      <p>
+        To do so, we provide OEMs and Tier-1s with Cyber Security Engineering
+        Lifecycle Services and cutting-edge in-vehicle, backend, mobile
+        services, and other security products. We work with our clients during
+        the entire lifecycle phases to ensure security is embedded into their
+        products. Selected by the world’s #1 automotive group, our unique Purple
+        approach has proven itself as the most effective in dealing with Cyber
+        Security risks, threat detection and response.
+      </p>
+      <a href="https://www.cymotive.com/">To see more</a>
+    </div>
+  );
+}
+
+export default CompanyInformation;
