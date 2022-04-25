335615d31c399dd47034dd41a334c963739d98a3
yakov cohen <yakovc1431@gmail.com>
2022-04-25 22:11:08 +0300

    # Add Data Interface

--- front/src/@types/custom.d.ts
+++ front/src/@types/custom.d.ts
@@ -1,3 +1,12 @@
 declare module '*.svg';
 declare module '*.mp4';
 declare module '*.png';
+
+declare namespace DataInterface {
+  interface data {
+    numberofreports: string;
+    numberofanomalies: string;
+    numberofvehicles: string;
+  }
+  type dataName = 'numberofreports' | 'numberofanomalies' | 'numberofvehicles';
+}
