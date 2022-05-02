87389abf0483d575eb37abaf940d634809d16d3f
yakov cohen <yakovc1431@gmail.com>
2022-05-02 20:06:31 +0300

    # Edit type

--- front/src/@types/custom.d.ts
+++ front/src/@types/custom.d.ts
@@ -4,9 +4,9 @@ declare module '*.png';
 
 declare namespace DataInterface {
   interface data {
-    numberofreports: string;
-    numberofanomalies: string;
-    numberofvehicles: string;
+    numberofreports: { name: string; value: number };
+    numberofanomalies: { name: string; value: number };
+    numberofvehicles: { name: string; value: number };
   }
   type dataName = 'numberofreports' | 'numberofanomalies' | 'numberofvehicles';
 }
